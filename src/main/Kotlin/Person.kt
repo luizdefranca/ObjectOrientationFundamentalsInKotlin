b4f4bc1342d6bb80099dffb7592c785c298855c9
LuizRamos <luizramospe@hotmail.com>
2021-03-11 12:40:53 -0300

    updated to apply the private modifier to set function of cpf property

--- src/main/Kotlin/Person.kt
+++ src/main/Kotlin/Person.kt
@@ -1,7 +1,14 @@
-class Person(var name: String = "Luiz",
-             var cpf: String = "123.123.123-00") {
+class Person {
+    var name: String = "Luiz"
+    var cpf: String = "123.123.123-00"
+    private set
+
 }
 
 fun main() {
-    println("test")
+    val luiz = Person()
+    println(luiz)
+    println(luiz.name)
+    println(luiz.cpf)
+
 }
